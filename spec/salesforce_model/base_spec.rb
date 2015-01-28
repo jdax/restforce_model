9648c77fad37ea1312c366eb9b5c67f1648e0ca7
Maggie <mepps1627@gmail.com>
2015-01-28 15:17:15 -0500

    Added test for initializing new object.

--- spec/salesforce_model/base_spec.rb
+++ spec/salesforce_model/base_spec.rb
@@ -1,1 +1,31 @@
 require_relative '../helper'
+
+include SalesforceModel
+
+describe SalesforceModel do
+
+
+  before :all do
+    Restforce.configure do |config|
+      #config.cache = Rails.cache
+      config.mashify = false
+    end
+    SalesforceModel.singleton_client = Restforce.new(:username => ENV['SALESFORCE_GLOBAL_USERNAME'],
+                                                     :password => ENV['SALESFORCE_GLOBAL_PASSWORD'],
+                                                     :security_token => ENV['SALESFORCE_GLOBAL_SECURITY_TOKEN'])
+    SalesforceModel.singleton_client.authenticate!
+
+    class Contact < SalesforceModel::Base
+      map_attributes :Name
+    end
+  end
+  describe 'Base' do
+    describe '#initialize' do
+      it 'creates a new object' do
+        contact = Contact.new(:Id => "01")
+        expect(contact.Id).to eq("01")
+      end
+    end
+  end
+
+end
