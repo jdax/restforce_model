aff0ce0128dd9a05ef61286b8fba84a030f31079
Maggie <mepps1627@gmail.com>
2015-01-29 10:17:31 -0500

    Added condition in case there are not mapped parent attributes to handle_parent_attributes.

--- lib/salesforce_model/attributes.rb
+++ lib/salesforce_model/attributes.rb
@@ -26,10 +26,12 @@ module SalesforceModel::Attributes
   alias_method :attributes=, :assign_attributes
 
   def handle_parent_attributes(attributes)
-    self.class.mapped_parent_attributes.each do |parent, fields|
-      parent_hash = attributes.delete(parent.to_sym)
-      fields.each do |field, mapped_to|
-        attributes[mapped_to] = parent_hash[field]
+    if self.class.mapped_parent_attributes
+      self.class.mapped_parent_attributes.each do |parent, fields|
+        parent_hash = attributes.delete(parent.to_sym)
+        fields.each do |field, mapped_to|
+          attributes[mapped_to] = parent_hash[field]
+        end
       end
     end
   end
