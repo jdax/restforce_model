be7e29881d223250b3024a203e75a2598ab17162
Piotr Steininger <piotr@socialdriver.com>
2015-02-27 19:41:18 -0500

    Fixed an issue where calling Model.new, with a mapped parent attribute would blow up, without a proper trace.

--- lib/salesforce_model/attributes.rb
+++ lib/salesforce_model/attributes.rb
@@ -27,9 +27,10 @@ module SalesforceModel::Attributes
 
   def handle_parent_attributes(attributes)
     self.class.mapped_parent_attributes.each do |parent, fields|
-      parent_hash = attributes.delete(parent.to_sym)
-      fields.each do |field, mapped_to|
-        attributes[mapped_to] = parent_hash[field]
+      unless parent_hash.blank?
+        fields.each do |field, mapped_to|
+          attributes[mapped_to] = parent_hash[field]
+        end
       end
     end
   end
