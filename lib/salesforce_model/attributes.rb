26c720f52c6358056baf963bd2800db60734d0e6
Piotr Steininger <piotr@socialdriver.com>
2015-02-27 20:15:42 -0500

    reinstated a line accidentally deleted

--- lib/salesforce_model/attributes.rb
+++ lib/salesforce_model/attributes.rb
@@ -27,6 +27,7 @@ module SalesforceModel::Attributes
 
   def handle_parent_attributes(attributes)
     self.class.mapped_parent_attributes.each do |parent, fields|
+      parent_hash = attributes.delete(parent.to_sym)
       unless parent_hash.blank?
         fields.each do |field, mapped_to|
           attributes[mapped_to] = parent_hash[field]
